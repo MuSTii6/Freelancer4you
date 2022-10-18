30ec9aad1b5e932f19938a7c6366e6f5072622d2
uzdilmus <uzdilmus@students.zhaw.ch>
2022-10-18 15:48:52 +0200

    1st commit

---
+++ src/test/java/ch/zhaw/testmongodb/TestmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.testmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
